73639200000b7a8594b0812d4bbf0b34936caccf
kaunjovi <kaunjovi@gmail.com>
2017-04-01 22:53:38 +0530

    got the springboot to boot up.

---
+++ src/main/java/fun/n/games/Application.java
@@ -0,0 +1,17 @@
+package fun.n.games;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	final static Logger logger = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		logger.debug("Kick off the SpringBoot application. ");
+		SpringApplication.run(Application.class, args);
+	}
+}
